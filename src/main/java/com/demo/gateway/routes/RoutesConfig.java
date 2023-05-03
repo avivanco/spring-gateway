5d8ade67071606cc134d95aa3820453d93002f4c
Alexis Vivanco V <alviv99@hotmail.com>
2023-05-03 18:52:22 -0500

    Con Predicados

--- src/main/java/com/demo/gateway/routes/RoutesConfig.java
+++ src/main/java/com/demo/gateway/routes/RoutesConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 
 //@Configuration
@@ -13,17 +14,36 @@ public class RoutesConfig {
     @Value("${uri.api.clientes}")
     private String clientesUri;
 
+    @Value("${uri.api.clientes2}")
+    private String clientesUri2;
+
     @Value("${uri.api.cuentas}")
     private String cuentasUri;
 
     @Bean
     public RouteLocator rutasDeClientes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("all_clientes", r -> r.path("/promerica/todos-los-clientes")
+                .route("all_clientes1", r -> r.path("/promerica/todos-los-clientes")
+                        .and().predicate(serverWebExchange -> {
+                            HttpHeaders headers = serverWebExchange.getRequest().getHeaders();
+                            String canal = headers.get("CANAL").get(0);
+                            return canal.equals("WEB");
+                        })
                         .filters(f -> f.rewritePath("/promerica/todos-los-clientes", "/v1/api/cliente/all")
                                 .addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY-SOLITUD")
                                 .addResponseHeader("X-GATEYWAY-RESPONSE-HEADER", "GATEWAY_RESPONSE"))
                         .uri(clientesUri)
+                )
+                .route("all_clientes2", r -> r.path("/promerica/todos-los-clientes")
+                        .and().predicate(serverWebExchange -> {
+                            HttpHeaders headers = serverWebExchange.getRequest().getHeaders();
+                            String canal = headers.get("CANAL").get(0);
+                            return canal.equals("MOBILE");
+                        })
+                        .filters(f -> f.rewritePath("/promerica/todos-los-clientes", "/v1/api/cliente/all")
+                                .addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY-SOLITUD")
+                                .addResponseHeader("X-GATEYWAY-RESPONSE-HEADER", "GATEWAY_RESPONSE"))
+                        .uri(clientesUri2)
                 )
                 .route("cliente_id", r -> r.path("/promerica/cliente/{id}")
                         .and().method(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT)
