45dcbb25ab6a6f7b811415d1ae6106407d81031c
Alexis Vivanco V <alviv99@hotmail.com>
2023-05-02 20:32:59 -0500

    Rutas de clientes

---
+++ src/main/java/com/demo/gateway/routes/RoutesConfig.java
@@ -0,0 +1,25 @@
+package com.demo.gateway.routes;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutesConfig {
+
+    @Value("${uri.api.clientes}")
+    private String clientesUri;
+
+    @Bean
+    public RouteLocator rutasDeClientes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("all_clientes", r -> r.path("/v1/api/cliente/all")
+                        .filters(f -> f.addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY-SOLITUD")
+                                .addResponseHeader("X-GATEYWAY-RESPONSE-HEADER", "GATEWAY_RESPONSE"))
+                        .uri(clientesUri)
+                )
+                .build();
+    }
+}
